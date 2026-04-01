b23f612273488fd1821916b823e1932e38d467df
yuexihuachen <554638047@qq.com>
2026-04-01 18:26:08 +0800

    feat: ziliao

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 https://lutaonan.com/blog/svelte/
+
+## three相关资源
+https://threejs.org/manual/#zh/useful-links
+
+## Three.js 基础入门课程
+https://threejs.org/manual/#zh/fundamentals
--- src/pages/create-text.tsx
+++ src/pages/create-text.tsx
@@ -1,6 +1,35 @@
+import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+
 const Index = () => {
-    return <div className="grid">
-        home
+    const domRef = useRef(null);
+    const contanierRef = useRef(null)
+
+    useEffect(() => {
+        // 场景（scene）、相机（camera）和渲染器（renderer）
+
+        // 场景（scene）
+        const scene = new THREE.Scene();
+        // 透视相机 PerspectiveCamera
+        // 视野范围（field of view）- 宽高比（aspect ratio）- 近裁剪面（near）和远裁剪面（far）
+        const camera = new THREE.PerspectiveCamera(75, contanierRef.current.clientWidth / contanierRef.current.clientHeight, 0.1, 1000);
+        // 渲染器（renderer）
+        const renderer = new THREE.WebGLRenderer();
+
+        // 设置渲染器尺寸 - 较低的分辨率渲染
+        renderer.setSize(contanierRef.current.clientWidth, contanierRef.current.clientHeight, false);
+  
+        domRef.current.appendChild(renderer.domElement);
+
+
+
+
+    }, [])
+
+    return <div ref={contanierRef} className="w-full h-full">
+        <div ref={domRef}>
+
+        </div>
     </div>
 }
 
