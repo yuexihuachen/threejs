e835a50945196951cb72dc86e0811310ea48c4fa
xingchendahai <554638047@qq.com>
2026-04-21 17:02:44 +0800

    feat: 进度三

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 https://www.youtube.com/watch?v=xJAfLdUgdc4&list=PLjcjAqAnHd1EIxV4FSZIiJZvsdrBc1Xho
 
 - 在 Three.js 中，“归一化值”（Normalized Value）通常指将某个数值范围映射到 [0,1] 或 [-1,1] 区间，或者将向量的长度缩放为 1（单位向量）。而“original normalized”可能是指“原始数据经过归一化后的值”，具体含义取决于上下文。
+
+https://medium.com/@gopisaikrishna.vuta/exploring-cameras-in-three-js-32e268a6bebd
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
 import stars from 'url:./img/stars.jpg';
 import nebula from 'url:./img/nebula.jpg';
+import vShader from './index.vert';
+import fShader from './index.frag';
+
+const monkeyUrl = new URL('./assets/monkey.glb', import.meta.url);
 
 const root = document.getElementById('root');
 // 场景（scene）、相机（camera）和渲染器（renderer）
@@ -135,6 +140,51 @@ scene.add(box2)
 box2.position.set(0, 15, 10)
 // box2.material.map = textureLoader.load(nebula)
 
+const plane2Geometry = new THREE.PlaneGeometry(10, 10, 10, 10);
+const plane2Material = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF,
+    wireframe: true
+})
+const plane2 = new THREE.Mesh(plane2Geometry, plane2Material);
+scene.add(plane2)
+plane2.position.set(10, 10, 15);
+
+plane2.geometry.attributes.position.array[0] -= 10 * Math.random();
+plane2.geometry.attributes.position.array[1] -= 10 * Math.random();
+plane2.geometry.attributes.position.array[2] -= 10 * Math.random();
+const lastPointZ = plane2.geometry.attributes.position.array.length - 1;
+plane2.geometry.attributes.position.array[lastPointZ] -= 10 * Math.random();
+
+const sphere2Geometry = new THREE.SphereGeometry(4);
+// const vShader = `
+//     void main() {
+//         gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+//     }
+// `;
+
+// const fShader = `
+//     void main() {
+//         gl_FragColor = vec4(0.5, 0.5, 1.0, 1.0);
+//     }
+// `;
+const sphere2Material = new THREE.ShaderMaterial({
+    vertexShader: vShader, // document.getElementById("vertexShader").textContent,
+    fragmentShader: fShader // document.getElementById("fragmentShader").textContent
+});
+const sphere2 = new THREE.Mesh(sphere2Geometry, sphere2Material);
+scene.add(sphere2);
+sphere2.position.set(-5, 10, 10);
+
+const assetLoader = new GLTFLoader();
+
+assetLoader.load(monkeyUrl.href, function(gltf) {
+    const model = gltf.scene;
+    scene.add(model);
+    model.position.set(-12, 4, 10)
+}, undefined, function(error) {
+    console.error(error)
+})
+
 const gui = new dat.GUI();
 
 const options = {
@@ -167,6 +217,17 @@ gui.add(options, 'decay', 0, 10)
 
 let step = 0;
 
+const mousePosition = new THREE.Vector2();
+
+window.addEventListener('mousemove', function(e) {
+    mousePosition.x = e.clientX / window.innerWidth * 2 - 1;
+    mousePosition.y = - e.clientY / window.innerHeight * 2 + 1;
+})
+
+const rayCaster = new THREE.Raycaster();
+const sphereId = sphere.id;
+box2.name = 'theBox';
+
 function animate(time) {
     box.rotation.x = time / 1000;
     box.rotation.y = time / 1000;
@@ -180,13 +241,36 @@ function animate(time) {
     spotLight.decay = options.decay;
     sLightHelper.update();
 
+    rayCaster.setFromCamera(mousePosition, camera);
+    const intersects = rayCaster.intersectObjects(scene.children);
+    // console.log(intersects)
+    for(let i = 0; i < intersects.length; i++) {
+        if (intersects[i].object.id === sphereId) {
+            intersects[i].object.material.color.set(0xFF0000)
+        }
+
+        if (intersects[i].object.name === 'theBox') {
+            intersects[i].object.rotation.x = time / 1000;
+            intersects[i].object.rotation.y = time / 1000;
+        }
+    }
+
+    plane2.geometry.attributes.position.array[0] = 10 * Math.random();
+    plane2.geometry.attributes.position.array[1] = 10 * Math.random();
+    plane2.geometry.attributes.position.array[2] = 10 * Math.random();
+    plane2.geometry.attributes.position.array[lastPointZ] = 10 * Math.random();
+    plane2.geometry.attributes.position.needsUpdate = true
     // 渲染器加载场景和相机
     renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate)
 
-
+window.addEventListener("resize", function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight)
+})
 
 
 
